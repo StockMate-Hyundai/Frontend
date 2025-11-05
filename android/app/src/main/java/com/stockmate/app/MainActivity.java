c1efe43b4171c57966e2138a8efb8cb3fed50d36
ksm <tpals555@daum.net>
2025-11-05 18:46:13 +0900

    [FEAT] navi

--- android/app/src/main/java/com/stockmate/app/MainActivity.java
+++ android/app/src/main/java/com/stockmate/app/MainActivity.java
@@ -1,11 +1,9 @@
 package com.stockmate.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.stockmate.app.StepCounterPlugin;
 
 public class MainActivity extends BridgeActivity {
-    static {
-        // StepCounter 플러그인 수동 등록
-        registerPlugin(StepCounterPlugin.class);
-    }
+    // @CapacitorPlugin 어노테이션이 있는 플러그인은 자동으로 스캔됩니다
+    // StepCounterPlugin은 @CapacitorPlugin(name = "StepCounter")로 선언되어 있으므로
+    // 별도의 수동 등록이 필요 없습니다
 }
